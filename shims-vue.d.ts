ac8b8d0d9b7993c17eca25388ab8ece83c08824d
Grace <476837464@qq.com>
2023-12-03 22:22:15 +0800

    添加类型声明

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -1,4 +1,10 @@
+declare module '*.svg'
 declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
 
 declare module '*.vue' {
     import type { DefineComponent } from 'vue'
